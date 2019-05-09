4b7a0e177a829fae0811637dda892fcd8ab6581f
Dyukusi <dyukusin@gmail.com>
2019-05-10 02:25:11 +0900

    add health check page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', {});
+});
+
+router.get('/health', function (req, res, next) {
+  res.render('health', {});
 });
 
 module.exports = router;
