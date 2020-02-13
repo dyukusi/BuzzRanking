dcc6fc417d71b59f45635f86e1c2d0ed64d79e8c
Dyukusi <dyukusin@gmail.com>
2020-02-13 19:00:31 +0900

    include 0 buzz product bundles in sitemap

--- bin/others/create_sitemap.js
+++ bin/others/create_sitemap.js
@@ -2,6 +2,9 @@ const appRoot = require('app-root-path');
 const fs = require('fs');
 const ejs = require('ejs');
 const sequelize = require(appRoot + '/db/sequelize_config');
+const ProductBundle = require(appRoot + '/models/product_bundle');
+const _ = require('underscore');
+const CONST = require(appRoot + '/lib/const.js');
 
 main()
   .then(() => {
@@ -12,13 +15,44 @@ main()
 async function main() {
   var SiteMapBase = fs.readFileSync(appRoot + '/views/sitemap_base.ejs', 'utf8');
 
+  var productBundleIdIntoLastUpdatedAtHash = {};
+
   var productBundleIdAndLastUpdatedAtRows = await sequelize.query(
-    'SELECT product_bundle_id AS productBundleId, MAX(date) AS lastUpdatedAt FROM product_tweet_stat WHERE product_bundle_id IN (SELECT id AS productBundleId FROM product_bundle WHERE id IN (SELECT DISTINCT product_bundle_id FROM product_tweet_stat WHERE tweet_count >= 1)) GROUP BY product_bundle_id;',
+    'SELECT product_bundle_id AS productBundleId, MAX(date) AS lastRecordedAt FROM product_tweet_stat WHERE product_bundle_id IN (SELECT id AS productBundleId FROM product_bundle WHERE id IN (SELECT DISTINCT product_bundle_id FROM product_tweet_stat WHERE tweet_count >= 0)) GROUP BY product_bundle_id;',
     {
       replacements: {},
       type: sequelize.QueryTypes.SELECT,
     }
   );
+
+  _.each(productBundleIdAndLastUpdatedAtRows, row => {
+    productBundleIdIntoLastUpdatedAtHash[row.productBundleId] = row.lastRecordedAt;
+  });
+
+  var productBundleIdAndLastTweetCollectedAtRows = await sequelize.query(
+    'SELECT product_bundle_id AS productBundleId, MAX(created_at) AS lastTweetCollectedAt FROM tweet_count_log GROUP BY product_bundle_id;',
+    {
+      replacements: {},
+      type: sequelize.QueryTypes.SELECT,
+    }
+  );
+
+  _.each(productBundleIdAndLastTweetCollectedAtRows, row => {
+    if (!productBundleIdIntoLastUpdatedAtHash[row.productBundleId]) {
+      productBundleIdIntoLastUpdatedAtHash[row.productBundleId] = row.lastTweetCollectedAt;
+    }
+  });
+
+  var allProductBundleIds = _.keys(productBundleIdIntoLastUpdatedAtHash);
+  var productBundleModels = await ProductBundle.findAll({
+    where: {
+      id: allProductBundleIds,
+      validityStatus: CONST.VALID_STATUS_IDS,
+    },
+  });
+  var targetProductBundleIds = _.pluck(productBundleModels, 'id');
+
+  console.log(targetProductBundleIds.length + " bundles were found");
 
   var rankingLastUpdatedAtDateStr = await (async () => {
     var row = (await sequelize.query(
@@ -33,7 +67,8 @@ async function main() {
   })();
 
   var sitemap = ejs.render(SiteMapBase, {
-    productBundleIdAndLastUpdatedAtRows,
+    targetProductBundleIds,
+    productBundleIdIntoLastUpdatedAtHash,
     rankingLastUpdatedAtDateStr,
 
     require: require,
